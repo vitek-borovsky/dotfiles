d97d07cf92a3a4855641e19140e99f6300dc72f4
vitek-borovsky <106488133+vitek-borovsky@users.noreply.github.com>
2022-06-22 20:27:42 +0200

    Update autoscript.sh

--- autoscript.sh
+++ autoscript.sh
@@ -21,7 +21,7 @@ fi
 
 usermod -aG wheel,video,audio,users,storage $target_user
 
-pacman -S neovim git alacritty firefox man qtile xorg-xinit xorg-server xf86-video-vmware xf86-video-fbdev xf86-video-vesa nitrogen noto-fonts curl dpkg zsh lua tmux udiskie neofetch htop locate picom htop jq dbus discord reflector cmatrix nemo ranger arandr --noconfirm
+pacman -S neovim git alacritty firefox man qtile xorg-xinit xorg-server xf86-video-vmware xf86-video-fbdev xf86-video-vesa nitrogen noto-fonts curl dpkg zsh lua tmux udiskie neofetch htop locate picom htop jq dbus discord reflector cmatrix nemo ranger arandr python-pip --noconfirm
 
 run_as_user "mkdir ~/Pictures"
 run_as_user "mkdir ~/Videos"
